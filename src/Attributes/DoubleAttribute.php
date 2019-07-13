da2a40ba0f0856acdeea62622db87e3ff63ab7bf
anthony <aamining2@gmail.com>
2019-07-13 16:16:28 +0200

    double attribute

---
+++ src/Attributes/DoubleAttribute.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Larapie\Actions\Attributes;
+
+use Faker\Generator;
+use Larapie\Actions\Attribute;
+
+class DoubleAttribute extends Attribute
+{
+    protected function rules()
+    {
+        return 'numeric';
+    }
+
+    public function cast($value)
+    {
+        return (double) $value;
+    }
+
+    public function factory(Generator $faker)
+    {
+        return $faker->randomFloat(2,0,1000);
+    }
+}
